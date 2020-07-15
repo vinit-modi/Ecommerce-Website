1450e184175e4ac30c9cb56d84a090f75da0f851
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-07-15 23:20:33 +0530

    Build Models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Customer, Product, Order, Order_Item, ShippingAddress
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Order_Item)
+admin.site.register(ShippingAddress)
