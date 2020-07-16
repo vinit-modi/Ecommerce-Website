80815ff4180acc662f99630c24a58f23448a7090
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-07-16 23:59:17 +0530

    render the checkout page

--- store/views.py
+++ store/views.py
@@ -21,6 +21,14 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete = False) #* Good Part
+        items = order.order_item_set.all()  #* Good Part
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
 
